ebf9ca4164f82be0426ae188ecb734b65ad09f4f
raccoon1619 <raccoon1619@gmail.com>
2020-05-17 20:48:04 +0300

    lab2

--- lab1/index.js
+++ lab1/index.js
@@ -1,169 +1,138 @@
 const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+const objectId = require("mongodb").ObjectID;
  
 const app = express();
 const bodyParser = require("body-parser");
+var jsonParser = bodyParser.json();
 
-var fs = require("fs");
-const urlencodedParser = bodyParser.urlencoded({extended: false});0
+const mongoClient = new MongoClient("mongodb://localhost:27017/", { useNewUrlParser: true });
  
-app.set("view engine", "ejs");
+let dbClient;
+
 app.use(express.static(__dirname + "/views"));
 
-app.post("/applyEdit/:noteId", urlencodedParser, function(request, response) {
-    if (!request.body) return response.sendStatus(400);
-
-    var id = request.params["noteId"];
-    var data = fs.readFileSync("notes.json", "utf8");
-    var notes = JSON.parse(data);
-    var index = -1;
-    // находим индекс заметки в массиве
-    for (var i = 0; i < notes.length; i++) {
-        if (notes[i].id == id){
-            index = i;
-            break;
-        }
-    }
-
-    if (index > -1) {
-        notes[index].caption = request.body.caption;
-        notes[index].content = request.body.content;
-        notes[index].status = request.body.status;
-        notes[index].date = new Date(Date.now()).toLocaleDateString('en-US');
-
-        data = JSON.stringify(notes);
-        fs.writeFileSync("notes.json", data);
-
-        response.redirect("/");
-    }
-    else {
-        response.status(404).send();
-    }
-});
-
-app.get("/filter/:status", function(request, response) { 
-      
-
-    if (request.params["status"].localeCompare("All") == 0) {
-        response.redirect("/");
-        return;
-    }
-
-    let statuses = new Map([
-        ["New", "Новая"],
-        ["Process", "Обрабатывается"],
-        ["Complete", "Завершено"]
-      ]);
-    
-    let status = statuses.get(request.params["status"]);
-
-    var filterNotes = [];
-    var data = fs.readFileSync("notes.json", "utf8");
-    var notes = JSON.parse(data);
-   
-    for (var i = 0; i < notes.length; i++) {
- 
-        if (status.localeCompare(notes[i].status) == 0)
-        {
-            filterNotes.push(notes[i]);
-        }
-    }
-
-    response.render("index", {
-        notes: filterNotes
+mongoClient.connect(function(err, client){
+    if (err) return console.log(err);
+    dbClient = client;
+    app.locals.collection = client.db("notesdb").collection("notes");
+    app.listen(3000, function(){
+        console.log("Сервер ожидает подключения...");
     });
 });
 
-app.post("/edit/:noteId", function(request, response) { 
-    var id = request.params["noteId"];
-    var data = fs.readFileSync("notes.json", "utf8");
-    var notes = JSON.parse(data);
-    var index = -1;
-    // находим индекс заметки в массиве
-    for (var i = 0; i < notes.length; i++) {
-        if (notes[i].id == id){
-            index = i;
-            break;
-        }
-    }
+app.get("/api/notes", function (request, response) {
+    
+    const collection = request.app.locals.collection;
+    collection.find({}).toArray(function(err, notes){
+         
+    if (err) return console.log(err);
+    response.send(notes);
+    });
+});
 
-    if (index > -1) {
-        response.render("edit", {
-        editNote: notes[index]
+app.get("/api/notes/:id", function(request, response) {        
+
+    const id = new objectId(request.params.id);
+    const collection = request.app.locals.collection;
+
+    collection.findOne({_id: id}, function(err, note){
+               
+        if (err) return console.log(err);
+        response.send(note);
+    });
+});
+
+app.post("/api/notes", jsonParser, function (request, response) {
+     
+    if(!request.body) return response.sendStatus(400);
+     
+    var noteCaption = request.body.caption;
+    var noteContent = request.body.content;
+    var noteStatus = request.body.status;
+    var noteDate = new Date(Date.now()).toLocaleDateString('en-US');
+    var note = {
+        caption: noteCaption, 
+        content: noteContent,
+        status: noteStatus,
+        date: noteDate
+    };    
+       
+    const collection = request.app.locals.collection;
+    collection.insertOne(note, function(err, result){
+               
+        if (err) return console.log(err);
+        response.send(note);
+    });
+});
+
+app.delete("/api/notes/:id", function(request, response){
+
+    const id = new objectId(request.params.id);
+    const collection = request.app.locals.collection;
+    collection.findOneAndDelete({_id: id}, function(err, result){
+               
+        if (err) return console.log(err);    
+        let note = result.value;
+        response.send(note);
+    });
+});
+
+app.put("/api/notes", jsonParser, function(request, response){
+      
+    if(!request.body) return response.sendStatus(400);
+    console.log(request.body);
+    const id = new objectId(request.body.id);
+    const noteCaption = request.body.caption;
+    const noteContent = request.body.content;
+    const noteStatus = request.body.status;
+    const noteDate = new Date(Date.now()).toLocaleDateString('en-US');
+       
+    const collection = request.app.locals.collection;
+    collection.findOneAndUpdate({_id: id}, { $set: 
+        {
+            caption: noteCaption,
+            content: noteContent,
+            status: noteStatus,
+            date: noteDate
+        }
+    },
+    { returnOriginal: false },
+    function(err, result) {               
+        if (err) return console.log(err);     
+        const note = result.value;
+        response.send(note);
+    });
+});
+
+
+app.get("/api/filter/:status", function(request, response) { 
+
+    const collection = request.app.locals.collection;
+    const status = request.params["status"];
+    if (status.localeCompare("Notes") == 0) {
+        collection.find({}).toArray(function(err, notes){            
+            if (err) return console.log(err);
+            response.send(notes);
         });
     }
     else {
-        response.status(404).send();
+        let statuses = new Map([
+            ["New", "Новая"],
+            ["Process", "Обрабатывается"],
+            ["Complete", "Завершено"]
+          ]);
+    
+        collection.find({ status: statuses.get(status)}).toArray(function(err, notes) {
+            if (err) return console.log(err);
+                response.send(notes);
+        });
     }
-
-});
- 
-app.post("/delete/:noteId", function(request, response) {
-
-    var id = request.params["noteId"];
-    var data = fs.readFileSync("notes.json", "utf8");
-    var notes = JSON.parse(data);
-    var index = -1;
-    // находим индекс заметки в массиве
-    for (var i = 0; i < notes.length; i++) {
-        if (notes[i].id == id){
-            index = i;
-            break;
-        }
-    }
-    if (index > -1) {
-        notes.splice(index, 1);
-        data = JSON.stringify(notes);
-        fs.writeFileSync("notes.json", data);
-
-        response.redirect("/");
-    }
-    else {
-        response.status(404).send();
-    }
+    
+    
 });
 
-app.post("/", urlencodedParser, function(request, response) {
-    if (!request.body) 
-        return response.sendStatus(400);
-
-        var data = fs.readFileSync("notes.json", "utf8");
-        var notes = JSON.parse(data);
-        var noteId;
-        var newDate = new Date(Date.now());
-        
-        if (notes[0] == null) {
-            noteId = 1;
-        } 
-        else {
-            var noteId = Math.max.apply(Math, notes.map(function(o){return o.id;}));
-            noteId++;
-        }
-
-        var newNote = {
-            id: noteId, 
-            caption: request.body.caption,
-            content: request.body.content,
-            status: request.body.status,
-            date: newDate.toLocaleDateString('en-US')
-        };
-         
-        notes.push(newNote);
-
-        data = JSON.stringify(notes);
-        // перезаписываем файл с новыми данными
-        fs.writeFileSync("notes.json", data);
-        response.redirect("/");
+process.on("SIGINT", () => {
+    dbClient.close();
+    process.exit();
 });
-
-app.get("/", function(request, response){ 
-
-    var content = fs.readFileSync("notes.json", "utf8");
-    var note = JSON.parse(content);
-
-    response.render("index", {
-        notes : note
-    });
-});
-
-
-app.listen(3000);
